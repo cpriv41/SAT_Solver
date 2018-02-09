496cce47b870714a496bdf6a4201af338f4f1a44
Caleb Privitera <32499212+cpriv41@users.noreply.github.com>
2018-02-09 11:17:33 -0500

    Update MainCalebPrivitera.java

--- MainCalebPrivitera.java
+++ MainCalebPrivitera.java
@@ -1,19 +1,16 @@
-
 package a1;
 
 import java.io.FileNotFoundException;
-import java.util.*;
+//import java.util.*;
 
 public class MainCalebPrivitera {
 	
 		 public static void main(String[] args) throws FileNotFoundException{
-			 
-		        String inFile = null;
-		        
-		    	if (args.length < 1) {
+			        
+  		    	if (args.length < 1) {
 					System.out.println("Must specify inputFileName from command line");
 					System.exit(1);
-		}
+		    	}
 		        if (args.length == 1) {
 		            run(args[0]);
 		      
@@ -36,10 +33,11 @@ public class MainCalebPrivitera {
 		        TimerCalebPrivitera timer = new TimerCalebPrivitera();
 		     
 		     
+		        System.out.println("Processing input file: " + input);
 		        timer.start();
 		        DPSolver.solve(DPSolver.readFormula(input));
 		        timer.stop();
-		        System.out.print("Time elapsed" + timer.getDuration());
+		        System.out.println("Time elapsed: " + timer.getDuration() + "ms");
 		    }
 
 }
